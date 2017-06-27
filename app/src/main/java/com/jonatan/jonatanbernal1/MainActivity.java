36098e90f12ef28df7aaae67613d0dce611ca525
Jonatan <jonatan.bernal@est.fib.upc.edu>
2017-06-27 18:48:33 +0200

    Comentario to flama

--- app/src/main/java/com/jonatan/jonatanbernal1/MainActivity.java
+++ app/src/main/java/com/jonatan/jonatanbernal1/MainActivity.java
@@ -3,26 +3,59 @@ package com.jonatan.jonatanbernal1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int numero;
     private static final String TAG = "MainActivity";
+
+    Button buttonOne, buttonTwo, buttonThree;
+    TextView texto;
+
+    private void setText(String text) {
+        texto.setText(text);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v(TAG,"onCreate");
         setContentView(R.layout.activity_main);
-        numero = 0;
+        buttonOne = (Button) findViewById(R.id.button1);
+        buttonTwo = (Button) findViewById(R.id.button2);
+        buttonThree = (Button) findViewById(R.id.button3);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "SOY EL BOTON 1 DESDE LA VARIABLE");
+            }
+        };
+        buttonOne.setOnClickListener(listener);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "SOY EL BOTON 1");
+                setText("SOY EL BOTON 1");
+            }
+        });
+
+        buttonTwo.setOnClickListener(this);
+        buttonThree.setOnClickListener(this);
+        texto = (TextView) findViewById(R.id.texto);
+        setText("HOLA");
 
     }
+
+
+
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.v(TAG, "onStart");
-        numero = 1;
+
 
     }
 
@@ -30,6 +63,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.v(TAG, "onDestroy");
-        numero = 4;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                Log.v(TAG, "SOY EL BOTON 1 EN THIS");
+                break;
+            case R.id.button2:
+                Log.v(TAG, "SOY EL BOTON 2");
+                setText("SOY EL BOTON 2");
+                break;
+            case R.id.button3:
+                Log.v(TAG, "SOY EL BOTON 3");
+                setText("SOY EL BOTON 3");
+                break;
+        }
+
+    }
+
+    public void mehanhechoclick (View v) {
+        Log.v(TAG, "SOY EL BOTON 4");
+        setText("SOY EL BOTON 4");
+
     }
 }
